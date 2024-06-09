40fd27c3f0e0d2b7a3199aa15506d091b23f1904
WindiKRockY <152081374+WindiKRockY@users.noreply.github.com>
2024-06-09 17:26:18 +0300

    Add files via upload

--- shooter_game.py
+++ shooter_game.py
@@ -4,11 +4,15 @@ from pygame import *
 init()
 font.init()
 font1 = font.SysFont("Impact", 100)
+font2 = font.SysFont("Impact",45)
 game_over_text = font1.render("GAME OVER", True, (150, 0, 0))
 mixer.init()
-mixer.music.load('space.ogg')
-# mixer.music.play()
-mixer.music.set_volume(0.2)
+mixer.music.load('spacee.ogg')
+mixer.music.play()
+mixer.music.set_volume(0.3)
+
+bullet_sound = mixer.Sound("fire.ogg")
+bullet_sound.set_volume(0.5)
 
 screen_info = display.Info()
 
@@ -23,9 +27,10 @@ bg = transform.scale(bg,(WIDTH,HEIGHT))
 bg_y1 = 0
 bg_y2 = -HEIGHT
 
-player_img = image.load("spaceship.png")
+player_img = image.load("spaceshipp.png")
 enemy_img = image.load("alien.png")
 all_sprites = sprite.Group()
+bullet_img = image.load("lazer.png")
 
 class Sprite(sprite.Sprite):
     def __init__(self, sprite_img, width, height, x, y):
@@ -42,9 +47,15 @@ class Player(Sprite):
 
         super().__init__(sprite_img, width, height, x, y)
         self.hp = 100
+        self.score = 0
         self.speed = 3
         self.bg_speed = 3
-        self.max_speed = 30
+        self.max_speed = 10
+
+    def shoot(self):
+        bullet_sound.play()
+        new_bullet = Bullet(bullet_img,22,30,self.rect.right- 40,self.rect.top)
+        new_bullet2 = Bullet(bullet_img,22,30,self.rect.left + 15,self.rect.top)
 
     def update(self):
         key_pressed = key.get_pressed()
@@ -78,15 +89,30 @@ class Enemy(Sprite):
         if self.rect.y > HEIGHT:
             self.kill()
 
+bullets = sprite.Group()
+class Bullet(Sprite):
+    def __init__(self, sprite_img, width, height,x,y):
+        super().__init__(sprite_img, width, height, x,y)
+        self.damage = 100
+        self.speed = 9
+        bullets.add(self)
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.bottom < 0:
+            self.kill()
 
 
-player = Player(player_img, 100, 70, 300, 300)
+player = Player(player_img, 90, 90, 300, 300)
 enemys = sprite.Group()
-enemy1 = Enemy(enemy_img,80,60)
+enemy1 = Enemy(enemy_img,80,75)
+enemy2 = Enemy(enemy_img,80,75)
+
 
 run = True
 finish = False
 bg_speed = 0
+
+score_text = font2.render(f"Score:{player.score}",True,(255,255,255))
 
 start_time = time.get_ticks()
 enemy_spawn_time = time.get_ticks()
@@ -98,10 +124,13 @@ while run:
         if e.type == KEYDOWN:
             if e.key == K_ESCAPE:
                 run = False
-
+            if e.key == K_SPACE:
+                player.shoot()
+    
     window.blit(bg,(0,bg_y1))
     window.blit(bg,(0,bg_y2))
-    
+    window.blit(score_text, (0,0))
+
     bg_y1 += player.bg_speed
     bg_y2 += player.bg_speed
     
@@ -116,7 +145,8 @@ while run:
 
     now = time.get_ticks()
     if now - enemy_spawn_time > spawn_interval:
-        enemy1 = Enemy(enemy_img,80,60)
+        enemy1 = Enemy(enemy_img,80,75)
+        enemy2 = Enemy(enemy_img,80,75)
         enemy_spawn_time = time.get_ticks()
         spawn_interval = randint(500,3500)
 
@@ -127,6 +157,12 @@ while run:
     all_sprites.draw(window)
     if not finish:
         all_sprites.update()
+
+    bulltes_colide = sprite.groupcollide(bullets,enemys,True,True,sprite.collide_mask)
+    for bullet in bulltes_colide:
+        player.score += 10
+        score_text = font2.render(f"Score:{player.score}",True,(255,255,255))
+
     if finish:
         window.blit(
             game_over_text, (WIDTH/2 - game_over_text.get_width()/2, HEIGHT/2 - game_over_text.get_height()/2))
